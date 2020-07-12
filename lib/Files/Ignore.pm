14690efd6494779f16711d207f297ef78d7ac7d3
Alon-L <2005alonlivne@gmail.com>
2020-07-13 00:44:41 +0300

    chore(Ignore): default value if the file was not found

--- lib/Files/Ignore.pm
+++ lib/Files/Ignore.pm
@@ -16,36 +16,41 @@ my $config_json;
 sub is_file_ignored {
     my ($file_name) = @_;
 
-    return grep { $_ eq $file_name} @{get_ignored_files()};
+    return grep {$_ eq $file_name} @{get_ignored_files()};
 }
 
 # Returns whether or not a given directory is ignored
 sub is_directory_ignored {
     my ($dir_name) = @_;
 
-    return grep { $_ eq $dir_name} @{get_ignored_directories()};
+    return grep {$_ eq $dir_name} @{get_ignored_directories()};
 }
 
 # Returns an array of the ignored directory names
 sub get_ignored_directories {
     my $config = read_config();
 
-    return $config->{directories};
+    return $config->{directories} || [];
 }
 
 # Returns an array of the ignored file names
 sub get_ignored_files {
     my $config = read_config();
 
-    return $config->{files};
+    return $config->{files} || [];
 }
 
 # Parses the JSON content inside the 'ignore.json' config file and loads them into the 'config' attribute
 sub read_config {
     return $config_json if (defined $config_json);
 
-    open my $fh, '<', IGNORE_CONFIG_PATH or die "Could not open ". IGNORE_CONFIG_PATH . " $!";
-    my $config_content = do { local $/; <$fh> };
+    # Set the config JSON to a default empty hash if the file was not found
+    open my $fh, '<', IGNORE_CONFIG_PATH or return $config_json = {};
+
+    my $config_content = do {
+        local $/;
+        <$fh>
+    };
     $config_json = JSON->new->decode($config_content);
 
     return $config_json;
